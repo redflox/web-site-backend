3edb87ebadd9561f9e337f5fd0eed1e0c4c9d3b0
Bryan Munoz <swsbmm@gmail.com>
2025-02-17 10:50:41 -0500

    global config for api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //configurar un prefijo para todas las rutas
+  app.setGlobalPrefix('api');
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 4000;
+
+  await app.listen(port);
+
+  console.log(`web-site-backend running on: http://localhost:${port}`);
 }
 bootstrap();
