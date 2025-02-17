fcf58a8aae5a90bc295ada9d37023c71ca4dc88e
Bryan Munoz <swsbmm@gmail.com>
2025-02-17 10:50:27 -0500

    added spotify module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SpotifyModule } from './spotify/spotify.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SpotifyModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
